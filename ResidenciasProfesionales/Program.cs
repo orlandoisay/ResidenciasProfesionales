fecbc7e8ae93b025360f4da664ef8ec19bc25095
JFernandoDJ <33406072+JFernandoDJ@users.noreply.github.com>
2018-04-06 14:32:24 -0500

    jajaja

--- ResidenciasProfesionales/Program.cs
+++ ResidenciasProfesionales/Program.cs
@@ -15,7 +15,7 @@ namespace ResidenciasProfesionales
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new VIEW.FrmAsignacionRevisores());
+            Application.Run(new VIEW.FrmInicioSesion());
         }
     }
 }
