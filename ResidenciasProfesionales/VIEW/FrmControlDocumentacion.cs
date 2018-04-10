9c39afbd2eb6c5ac7af6275166d1a6ff06726019
Andres97gt <36773198+Andres97gt@users.noreply.github.com>
2018-04-09 23:44:31 -0500

    Formulario para: CU_05.- Control de documentación.

---
+++ ResidenciasProfesionales/VIEW/FrmControlDocumentacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ResidenciasProfesionales.VIEW
+{
+    public partial class FrmControlDocumentacion : Form
+    {
+        public FrmControlDocumentacion()
+        {
+            InitializeComponent();
+            insertarDocumentos();
+        }
+
+        public void insertarDocumentos() {
+            tablaDocumentos.Rows.Add("Cronograma de actividades");
+            tablaDocumentos.Rows.Add("Informe");
+            tablaDocumentos.Rows.Add("Resumen ejecutivo");
+            tablaDocumentos.Rows.Add("CD");
+            tablaDocumentos.Rows.Add("Asesorias");
+            tablaDocumentos.Rows.Add("Documento de evaluación");
+        }
+    }
+}
