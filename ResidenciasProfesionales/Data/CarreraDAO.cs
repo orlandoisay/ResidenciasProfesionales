5b57e3679ac0e587c9c67554977fedb645d4d01b
orlandoisay <orlandoisay@gmail.com>
2018-04-11 17:28:31 -0500

    Creando DAO y POJO de carrera

---
+++ ResidenciasProfesionales/Data/CarreraDAO.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using ResidenciasProfesionales.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciasProfesionales.DATA
+{
+    public class CarreraDAO
+    {
+        public static CarreraPOJO ObtenerUno(int id)
+        {
+            try
+            {
+                Conexion con = new Conexion();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM carrera WHERE id = @P0");
+                cmd.Parameters.AddWithValue("@P0", id);
+
+                DataTable dt = con.ejecutarConsulta(cmd);
+
+                if (dt.Rows.Count != 1)
+                    return null;
+
+                return DataRowAObjeto(dt.Rows[0]);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                if (Conexion.conexion != null)
+                    Conexion.conexion.Close();
+            }
+        }
+
+        public static CarreraPOJO DataRowAObjeto(DataRow dr)
+        {
+            return new CarreraPOJO(
+                int.Parse(dr["ID"].ToString()),
+                dr["Nombre"].ToString()
+            );
+        }
+    }
+}
---
+++ ResidenciasProfesionales/MODEL/CarreraPOJO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciasProfesionales.MODEL
+{
+    public class CarreraPOJO
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+
+        public CarreraPOJO() { }
+        public CarreraPOJO(int id, string nombre)
+        {
+            ID = id;
+            Nombre = nombre;
+        }
+    }
+}
