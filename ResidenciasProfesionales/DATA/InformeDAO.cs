2b58018471abf1b656c4957672d14f12aef6b4e1
orlandoisay <orlandoisay@gmail.com>
2018-04-11 17:29:48 -0500

    Agregando DAO y POJO de Informe

---
+++ ResidenciasProfesionales/DATA/InformeDAO.cs
@@ -0,0 +1,24 @@
+﻿using ResidenciasProfesionales.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciasProfesionales.DATA
+{
+    public class InformeDAO
+    {
+        /// <summary>
+        /// Obtiene la lista de informes por periodo.
+        /// </summary>
+        /// <param name="anio"></param>
+        /// <param name="periodo"></param>
+        /// <param name="aprobados"></param>
+        /// <returns></returns>
+        public static List<ResidenciaPOJO> ObtenerInformesPorPeriodo(int anio, string periodo, int idDocente, bool aprobados)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ResidenciasProfesionales/MODEL/InformePOJO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciasProfesionales.MODEL
+{
+    public class InformePOJO
+    {
+        public enum Dictamen
+        {
+            APROBADO,
+            RECHAZADO,
+            PENDIENTE
+        }
+
+        public AlumnoPOJO Alumno { get; set; }
+        public Dictamen DictamenAsesor { get; set; }
+        public Dictamen DictamenRevisor1 { get; set; }
+        public Dictamen DictamenRevisor2 { get; set; }
+
+        /// <summary>
+        /// Si hay al menos un dictamen pendiente, será pendiente.
+        /// En caso contrario será igual al valor por mayoría.
+        /// </summary>
+        public Dictamen DictamenFinal
+        {
+            get
+            {
+                if (DictamenAsesor == Dictamen.PENDIENTE ||
+                   DictamenRevisor1 == Dictamen.PENDIENTE ||
+                   DictamenRevisor2 == Dictamen.PENDIENTE)
+                    return Dictamen.PENDIENTE;
+
+                int aprobados = (DictamenAsesor == Dictamen.PENDIENTE ? 1 : 0) +
+                                (DictamenRevisor1 == Dictamen.PENDIENTE ? 1 : 0) +
+                                (DictamenRevisor2 == Dictamen.PENDIENTE ? 1 : 0);
+
+                return aprobados >= 2 ? Dictamen.APROBADO : Dictamen.RECHAZADO;
+            }
+        }
+
+        public InformePOJO() { }
+
+        public InformePOJO(AlumnoPOJO alumno, Dictamen dictamenRevisor1, Dictamen dictamenRevisor2)
+        {
+            Alumno = alumno;
+            DictamenRevisor1 = dictamenRevisor1;
+            DictamenRevisor2 = dictamenRevisor2;
+        }
+    }
+}
