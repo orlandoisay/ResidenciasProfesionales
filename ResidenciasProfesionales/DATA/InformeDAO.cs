d37f4c206c39a333c7620d13f3c6785fa21eb95a
orlandoisay <orlandoisay@gmail.com>
2018-04-11 21:16:20 -0500

    Corrigiendo DAO

--- ResidenciasProfesionales/DATA/InformeDAO.cs
+++ ResidenciasProfesionales/DATA/InformeDAO.cs
@@ -16,7 +16,7 @@ namespace ResidenciasProfesionales.DATA
         /// <param name="periodo"></param>
         /// <param name="aprobados"></param>
         /// <returns></returns>
-        public static List<ResidenciaPOJO> ObtenerInformesPorPeriodo(int anio, string periodo, int idDocente, bool aprobados)
+        public static List<InformePOJO> ObtenerInformesPorPeriodo(int anio, string periodo, int idDocente, bool aprobados)
         {
             throw new NotImplementedException();
         }
