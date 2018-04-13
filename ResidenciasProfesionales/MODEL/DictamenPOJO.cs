c71aeeb993195bd798e9971fc3807f2b61e66db3
Andres97gt <36773198+Andres97gt@users.noreply.github.com>
2018-04-13 07:20:11 -0500

    Modificación del POJO y el DAO de dictamen  así como el Frm mostrar datos.

--- ResidenciasProfesionales/MODEL/DictamenPOJO.cs
+++ ResidenciasProfesionales/MODEL/DictamenPOJO.cs
@@ -12,18 +12,20 @@ namespace ResidenciasProfesionales.MODEL
         public int IdResidencia { get; set; }
         public String IdDocente { get; set; }
         public String Tipo { get; set; }
+        public String Estatus { get; set; }
         public String Comentario { get; set; }
-        public float Calificacion { get; set; }
+        public int Calificacion { get; set; }
         public DateTime Fecha { get; set; }
 
         public DictamenPOJO() { }
 
-        public DictamenPOJO(int id, int idResidencia, String idDocente, String tipo, String comentario, 
-            float calificacion, DateTime fecha) {
+        public DictamenPOJO(int id, int idResidencia, String idDocente, String tipo, String estatus, 
+            String comentario, int calificacion, DateTime fecha) {
             ID = id;
             IdResidencia = idResidencia;
             IdDocente = idDocente;
             Tipo = tipo;
+            Estatus = estatus;
             Comentario = comentario;
             Calificacion = calificacion;
             Fecha = fecha;
