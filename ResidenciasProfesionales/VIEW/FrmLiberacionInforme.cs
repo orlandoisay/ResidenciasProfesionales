21741795c1edc613bc3629908cea2c1dcde7821f
orlandoisay <orlandoisay@gmail.com>
2018-04-15 22:26:02 -0500

    Terminando el caso de uso

--- ResidenciasProfesionales/VIEW/FrmLiberacionInforme.cs
+++ ResidenciasProfesionales/VIEW/FrmLiberacionInforme.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ResidenciasProfesionales.DATA;
+using ResidenciasProfesionales.MODEL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,9 +13,75 @@ namespace ResidenciasProfesionales.VIEW
 {
     public partial class FrmLiberacionInforme : Form
     {
-        public FrmLiberacionInforme()
+        private String IdDocente;
+        private String Matricula;
+        private String Rol;
+        private DictamenPOJO Dictamen;
+
+        public FrmLiberacionInforme(String idDocente, String matricula)
         {
+            IdDocente = idDocente;
+            Matricula = matricula;
+            Rol = RolDAO.ObtenerRol(matricula, idDocente).Rol;
+            Dictamen = DictamenDAO.ObtenerDictamen(idDocente, matricula, "Aprobacion" + Rol);
+
             InitializeComponent();
+            CargarDatosAlumno();
+
+            if (Dictamen != null)
+                CargarDatosDictamen();
         }
+        private void btnEvaluar_Click(object sender, EventArgs e)
+        {
+            GuardarDatos();
+        }
+
+        private void CargarDatosAlumno()
+        {
+            var alumno = AlumnoDAO.ObtenerAlumno(Matricula);
+
+            txtNoControl.Text = alumno.Matricula;
+            txtNombre.Text = alumno.NombreCompleto;
+            txtCarrera.Text = CarreraDAO.ObtenerUno(alumno.Carrera).Nombre;
+            txtSemestre.Text = alumno.Semestre.ToString();
+        }
+        private void CargarDatosDictamen()
+        {
+            btnEvaluar.Enabled = false;
+            txtComentarios.Enabled = false;
+            txtComentarios.Text = Dictamen.Comentario;
+        }
+        private bool ValidarDatos()
+        {
+            return true;
+        }
+        private void GuardarDatos()
+        {
+            if (!ValidarDatos())
+                return;
+
+            DialogResult dr = MessageBox.Show("Continuar con el guardado", "Info",
+                                              MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dr == DialogResult.No)
+                return;
+
+            var solicitud = SolicitudDAO.ObtenerSolicitud(Matricula);
+            var idResidencia = solicitud.IdResidencia;
+            var tipo = (Rol == "Asesor") ? "LiberacionAsesor" : "LiberacionRevisor";
+
+            DictamenDAO.InsertarDictamen(
+                new DictamenPOJO(-1,
+                                 idResidencia,
+                                 IdDocente,
+                                 tipo,
+                                 "Aceptado",
+                                 txtComentarios.Text,
+                                 0,
+                                 DateTime.Now));
+
+            Close();
+        }
+
     }
 }
