1d463c22c6907c94429d6908ca8a586ac048085b
orlandoisay <orlandoisay@gmail.com>
2018-04-15 21:25:03 -0500

    Se creo rol

---
+++ ResidenciasProfesionales/DATA/RolDAO.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using ResidenciasProfesionales.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciasProfesionales.DATA
+{
+    public class RolDAO
+    {
+        public static RolPOJO ObtenerRol(String idAlumno, String idDocente)
+        {
+            try
+            {
+                Conexion con = new Conexion();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM rolDocente WHERE IdAlumno LIKE @P0 AND IdDocente LIKE @P1");
+                cmd.Parameters.AddWithValue("@P0", idAlumno);
+                cmd.Parameters.AddWithValue("@P1", idDocente);
+
+                DataTable dt = con.ejecutarConsulta(cmd);
+
+                if (dt.Rows.Count != 1)
+                    return null;
+
+                return DataRowAObjeto(dt.Rows[0]);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                if (Conexion.conexion != null)
+                    Conexion.conexion.Close();
+            }
+        }
+
+        public static RolPOJO DataRowAObjeto(DataRow dr)
+        {
+            return new RolPOJO(
+                dr["IdAlumno"].ToString(),
+                dr["IdDocente"].ToString(),
+                dr["Rol"].ToString()
+            );
+        }
+    }
+}
---
+++ ResidenciasProfesionales/MODEL/RolPOJO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciasProfesionales.MODEL
+{
+    public class RolPOJO
+    {
+        public String IdAlumno { get; set; }
+        public String IdDocente { get; set; }
+        public String Rol { get; set; }
+
+        public RolPOJO() { }
+        public RolPOJO(String idAlumno, String idDocente, String rol)
+        {
+            IdAlumno = idAlumno;
+            IdDocente = idDocente;
+            Rol = rol;
+        }
+    }
+}
